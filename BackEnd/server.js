abef8daf38a1863d9da00dc974c47b95c6ffcb5f
fahim amiri <famiri1@mail.sfsu.eduu>
2024-06-22 15:47:24 -0700

    amiri's profile update

---
+++ BackEnd/server.js
@@ -0,0 +1,59 @@
+
+const createError = require("http-errors");
+const express = require("express");
+
+const livereload = require("livereload");
+const path = require("path");
+
+const requestTime = require("./middleware/request-time");
+const router = express.Router();
+const app = express();
+
+app.use(requestTime);
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "static" directory in the "backend" folder
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "static")));
+
+const rootRoutes = require("./routes/root");
+app.use("/", rootRoutes);
+
+// Default route
+// Default route
+app.get("/", (request, response) => {
+  response.render('index');
+});
+
+// Route for /contact
+app.get("/contact", (request, response) => {
+  response.render('contact');
+});
+app.get("/about", (request, response) => {
+  response.render('about');
+});
+app.get("/team", (request, response) => {
+  response.render('team');
+});
+
+// Middleware to handle 404 errors
+app.use((request, response, next) => {
+    next(createError(404));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+if (process.env.NODE_ENV === "development") {
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+    liveReloadServer.server.once("connection", () => {
+      setTimeout(() => {
+        liveReloadServer.refresh("/");
+      }, 100);
+    });
+  }
